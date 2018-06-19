3f4fc727f0c410f51dfc150d679c4e316123fc8b
Paul Mayer <paul@paul-mayer.eu>
2018-06-19 12:16:24 +0200

    test websocket in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,9 @@
-import {Controller, Get, HttpCode, Param, Post, Render} from '@nestjs/common';
-import {UserProvider} from './user.provider';
-import {User} from './user.entity';
+import {Controller, Get, HttpCode, Param, Post, Render} from "@nestjs/common";
+import {UserProvider} from "./user.provider";
+import {User} from "./user.entity";
+import {SubscribeMessage, WsResponse} from "@nestjs/websockets";
+import {from, Observable} from "rxjs/index";
+import {map} from "rxjs/internal/operators";
 
 @Controller('users')
 export class UserController {
@@ -28,6 +31,14 @@ export class UserController {
     @HttpCode(204)
     create() {
         return 'This action adds a new cat';
+    }
+
+    @SubscribeMessage('event')
+    onEvent(client, data): Observable<WsResponse<number>> {
+        const event = 'event';
+        const response = [1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 9];
+
+        return from(response).pipe(map(res => ({event, data: res})));
     }
 
 }
