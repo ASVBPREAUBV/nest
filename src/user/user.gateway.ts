5ae5cd52c9fa9c2ac0c44318767fddfa06f35f8c
Paul Mayer <paul@paul-mayer.eu>
2018-07-21 19:04:16 +0200

    ws return user

--- src/user/user.gateway.ts
+++ src/user/user.gateway.ts
@@ -3,13 +3,14 @@ import {from, Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {host_websocket_port} from "../config";
 import {namespace} from "./user.config";
+import {User} from "./user.entity";
 
 @WebSocketGateway(host_websocket_port)
 export class UserGateway {
 
     @SubscribeMessage(namespace)
-    onEvent(client, data): Observable<WsResponse<number>> {
-        const response = [99, 98, 97];
+    onEvent(client, data): Observable<WsResponse<User>> {
+        const response = [{id: 999, email: 'test@example', password_hash: 'pw'}];
         console.log('UserGateway onEvent', data);
 
         return from(response).pipe(map(res => ({event: namespace, data: res})));
