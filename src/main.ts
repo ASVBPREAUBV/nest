daf8a90b43e1f21396950b7738cf48617896e894
Paul Mayer <paul@paul-mayer.eu>
2018-07-28 15:03:44 +0200

    init winston logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
+import {logger} from "./config/logger";
 
 async function bootstrap() {
 
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+       logger,
+    });
 
     app.useStaticAssets(__dirname + '/static');
     app.setBaseViewsDir(__dirname + '/views');
